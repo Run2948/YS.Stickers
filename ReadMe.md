150bc2bf6cde1997e17bacb8f0fd937eafea72e7
少年那已不再 <15997352948@163.com>
2018-10-10 16:07:10 +0800

    上传项目源码

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# 图片贴纸插件
+
+一款仿手机相册图片编辑功能里面的“贴纸”功能，能够在一张背景图片上添加任意贴纸功能
+
+## 项目依赖
+
+该插件依赖于[jquery.image-label.js]('https://github.com/ekoz/jquery.image-label.js/blob/master/README.md') 图片标签插件，用于描述图片中的特征事物，主要应用场景是图像识别后的特征展示。
+
+## 用法：
+
+该项目是基于`jquery.image-label.js`来自定义开发，用法和`jquery.image-label.js`保持一致，只需要额外添加自定义方法和属性。参考：[jquery.image-label.js]('https://github.com/ekoz/jquery.image-label.js/blob/master/README.md')的用法。
+
+## 演示：
+
+![图片演示](./screenshots/1.png)
+了解更多详情[点击这里](https://github.com/Run2948/YS.Stickers/index.html)
+
+### 上传本地项目到 Git
+
+* 1、初始化仓库 git init
+* 2、添加文件目录到git仓库 git add . (. 是全部文件，文件名 可指定文件)
+* 3、上传到git仓库命令 git commit -m "可写注释内容"
+* 4、连接远程仓库连接命令 git remote add origin `https://github.com/你的github的用户名/test.git`（这里是版本库URL）
+* 5、推送到分支(master)的命令 git push -u origin master（分支名字master为主分支）
---
+++ jquery.image-label.js
@@ -0,0 +1,180 @@
+/**
+ * 图片中插入特征标签
+ * @author zhujinrun
+ * @since 2018-10-10 10:00
+ */
+(function($){
+
+    var area = null;
+
+    var builder = {
+        debug: function(o){
+            if (window.console && window.console.log){
+                console.log(o);
+            }
+        },
+        fire: function(key, elem, opts){
+            if (typeof key == 'string') {
+                if (this[key]){
+                    return this[key](elem, opts);
+                }else{
+                    this.debug('function [' + key + '] undefined');
+                }
+            }
+        },
+        /**
+         * 初始化插件
+         * @param elem img 元素
+         */
+        init: function(elem){
+            if ($(elem).parent('.kbs-label-area').length==0){
+                $(elem).wrap('<div class="kbs-label-area"></div>');
+                area = $(elem).parent('.kbs-label-area');
+                this.debug('image-label plugins initialized.');
+                //面板监听
+                area.on({
+                    'drop': function(e){
+                        e.preventDefault();
+                        e.stopPropagation();
+                        var dataTransfer = e.dataTransfer || e.originalEvent.dataTransfer;
+                        var data = dataTransfer.getData("Text");
+                        if (data!='kbs-drag-active') return false;
+                        var _label = $('.' + data);
+                        _label.css({
+                            left: e.clientX || e.originalEvent.clientX,
+                            top: e.offsetY || e.originalEvent.clientY
+                        })
+                        _label.attr('_top', e.offsetY || e.originalEvent.clientY);
+                        _label.attr('_left', e.clientX || e.originalEvent.clientX);
+                        $(this).append($('.' + data));
+                        $('.' + data).removeClass('kbs-drag-active');
+                    },
+                    'dragover': function(e){
+                        e.preventDefault();
+                    }
+                });
+                //面板标签监听
+                area.on('dragstart', '.kbs-label', function(e){
+                    var _this = this;
+                    $(_this).addClass('kbs-drag-active');
+                    var dataTransfer = e.dataTransfer || e.originalEvent.dataTransfer;
+                    dataTransfer.setData("Text", 'kbs-drag-active');
+                });
+                area.on('dblclick', '.kbs-label', function(e){
+                    console.log(this);
+                    var _this = this;
+                    if (window.confirm('确定要删除当前标记吗?如果确定，请手动点击【保存】按钮来保存删除状态保存。否则只会将当前标记从图片中隐藏。')){
+                        $(this).remove();
+                    }
+                });
+            }
+        },
+        /**
+         * 加载 json
+         * @param arr [{"text":"主队","index":"1","top":"139","left":"778"},{"text":"客队","index":"2","top":"98","left":"483"}]
+         */
+        loadData: function(elem, opts){
+            $(opts.data).each(function(i, item){
+                var _labelHtml = '<div _index="' + item.index + '" class="kbs-label kbs-label-arrow kbs-label-arrow-left kbs-label-black kbs-label-' + item.index +  '" draggable="true" style="top:' + item.top + 'px;left:' + item.left + 'px" _top="' + item.top + '" _left="' + item.left + '">' + item.text + '</div>';
+                area.append(_labelHtml);
+            });
+        },
+        /**
+         * 获取标签，返回 json arr
+         */
+        getData: function(){
+            var _arr = [];
+            var _labelArr = area.find('.kbs-label');
+            $(_labelArr).each(function(i, _label){
+                var _json = {};               
+                _json.text = $(_label).html();
+                _json.index = $(_label).attr('_index');
+                _json.top = $(_label).attr('_top');
+                _json.left = $(_label).attr('_left');
+                _arr.push(_json);
+            });
+            return _arr;
+        },
+        create_text: function(elem, opts){
+            var _ind = area.find('.kbs-label').length;
+            _ind++;
+            var _top = $(elem).get(0).offsetTop + 10 + _ind * 20;
+            var _left = $(elem).get(0).offsetLeft + 30;
+            opts = $.extend({
+                text: '标记',
+                top: _top,
+                left: _left
+            }, opts);
+            var _class = 'kbs-label-' + _ind;
+            var _text = opts.text;
+            var _labelHtml = '<div _index="' + _ind + '" class="kbs-label kbs-label-arrow kbs-label-arrow-left kbs-label-black ' + _class +  '" draggable="true">' + _text + '</div>';
+            area.append(_labelHtml);
+            var _label = area.find('.' + _class);
+            _label.css({
+                top: opts.top,
+                left: opts.left
+            });
+            _label.attr('_top', opts.top);
+            _label.attr('_left', opts.left);
+        },
+        create_icon:function(elem, opts){
+            var _ind = area.find('.kbs-label').length;
+            _ind++;
+            var _top = $(elem).get(0).offsetTop + 10 + _ind * 30;
+            var _left = $(elem).get(0).offsetLeft + 30;
+            opts = $.extend({
+                text: './icons/xfx.png',
+                top: _top,
+                left: _left
+            }, opts);
+            var _class = 'kbs-label-' + _ind;
+            var _text = opts.text;
+            var _labelHtml = '<div _index="' + _ind + '" class="kbs-label kbs-label-arrow kbs-label-arrow-left kbs-label-black ' + _class +  '" draggable="true"><img src="' + _text + '" alt="icon"></div>';
+            //var _labelHtml = '<div _index="' + _ind + '" class="kbs-label ' + _class +  '" draggable="true"><img src="' + _text + '" alt="icon"></div>';
+            area.append(_labelHtml);
+            var _label = area.find('.' + _class);
+            _label.css({
+                top: opts.top,
+                left: opts.left
+            });
+            _label.attr('_top', opts.top);
+            _label.attr('_left', opts.left);            
+        },
+        hide: function(elem, opts){
+            area.find('.kbs-label').hide();
+        },
+        show: function(elem, opts){
+            area.find('.kbs-label').show();
+        },
+        clear: function(elem, opts){
+            var _labelArr = area.find('.kbs-label');
+            if (_labelArr.length > 0){
+                if (window.confirm('确定要删除全部标记吗?')){
+                    $(_labelArr).each(function(i, _label){
+                        //console.log(_label);
+                        $(_label).remove();
+                    });
+                }
+            }    
+        }
+    }
+
+
+    $.fn.extend({
+        /**
+         *
+         * @param key
+         * @param opts
+         */
+        imageLabel: function(key, opts){
+            var _this = this;
+            if (key==undefined || typeof key!='string'){
+                builder.init(_this);
+            }else{
+                if ($(_this).parent('.kbs-label-area').length==0) builder.init(_this);
+                return builder.fire(key, _this, opts);
+            }
+        }
+    });
+
+})(jQuery);
